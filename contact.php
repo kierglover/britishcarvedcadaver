df0d25f718f38d97cbbe5625b9f1adf44190bbf6
Kieran Glover <kier.p.glover@gmail.com>
2016-09-09 16:23:50 +0100

    changed nav, continued changeover to sass

--- contact.php
+++ contact.php
@@ -36,22 +36,38 @@
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
   
-    <!-- NAV -->
-    
-        <div id="mySidenav" class="sidenav">
-            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-            <a href="index.html">Home</a>
-            <a href="about.html">About</a>
-            <a href="locations.html">Locations</a>
-            <a href="contact.html">Contact</a>
-        </div>
-        
-    <!-- /.NAV -->
-       
-    <div class="slide-out-nav">
-        <!-- Use any element to open the sidenav -->
-        <span onclick="openNav()"><i class="material-icons md-light">menu</i></span>
-    </div>
+ <!-- NAV -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+              <a class="navbar-left" href="#"><img src="img/E%20Crook%20Transi%20Logo.jpg" height="30px"></a>
+            </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#">Home<span class="sr-only">(current)</span></a></li>
+                <li class="dropdown">
+                  <a href="about.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="sculpting.html">Sculpting</a></li>
+                    <li><a href="anatomy.html">Anatomy</a></li>
+                    <li><a href="theology.html">Theology</a></li>
+                  </ul>
+                </li>
+                <li><a href="locations.html">Locations</a></li>
+                <li><a href="contact.php">Contact</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav><!-- /.NAV -->
     
     <!-- Content Section -->
         <div class="container-fluid">
